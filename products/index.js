93a865124e328335b743638667f5fa9fb2785f8d
Arjan Aswal <arjanaswal09@gmail.com>
2022-04-28 00:25:54 +0530

    Integrate RabbitMQ and create buy products route

--- products/index.js
+++ products/index.js
@@ -3,7 +3,18 @@ require('express-async-errors');
 const sequelize = require('./database');
 const isAuthenticated = require('./isAuthenticated');
 const Product = require('./product.model');
+const amqp = require('amqplib');
+const Sequelize = require('sequelize');
 
+let channel;
+
+async function connect() {
+  const amqpServer = process.env.RABBITMQ_URL;
+  const connection = await amqp.connect(amqpServer);
+  channel = await connection.createChannel();
+  await channel.assertQueue('PRODUCT');
+}
+connect();
 const app = express();
 
 app.use(express.json());
@@ -32,6 +43,32 @@ app.post('/products', isAuthenticated, async (req, res) => {
   });
 
   res.status(200).json(product);
+});
+
+app.post('/products/buy', isAuthenticated, async (req, res) => {
+  const { ids } = req.body;
+  const products = await Product.findAll({
+    where: {
+      id: {
+        [Sequelize.Op.in]: ids,
+      },
+    },
+  });
+  let order;
+
+  channel.sendToQueue(
+    'ORDER',
+    Buffer.from(
+      JSON.stringify({
+        products,
+        userEmail: req.user.email,
+      })
+    )
+  );
+  await channel.consume('PRODUCT', data => {
+    order = JSON.parse(data.content);
+  });
+  res.json(order);
 });
 
 sequelize.sync();
